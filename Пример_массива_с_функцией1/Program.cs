861fabb4ddd31d4c68244598c3f9059ee8a5cc6e
Kostas <langas.kostya@mail.ru>
2022-07-15 17:40:23 +0500

    Методы добавления чисел в массив и вывода на экран содержимого массива

---
+++ Пример_массива_с_функцией1/Program.cs
@@ -0,0 +1,29 @@
+﻿void Fillarray(int[]collection)     // Метод (функция) заполнение массива рандомными числами в пределах периода
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(0, 10);
+        index++;
+    }
+
+}
+
+
+void Printarray(int[] col)      // Метод (функция) вывода на экран всех чисел из массива
+{
+    int count = col.Length;
+    int position = 0;
+
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int [10]; // размер массива равный 10
+
+Fillarray(array);
+Printarray(array);
